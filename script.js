27568601fd65c75ea62071cc1fd97212e7ca178d
dima <>
2025-06-19 18:05:22 +0300

    Add wallet and price calculation form with event handling

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // result = (0.1 + 0.2).toFixed(1)
 // console.log(result)
 // console.log(typeof result)
+// let? и Number?  и typeof
 
 let result = Number((0.1 + 0.2).toFixed(1))
 console.log(result)
@@ -32,3 +33,20 @@ Form.addEventListener("submit", (event) => {
   console.log(files);
 })
 
+const Form2 = document.getElementById("form-2")
+const Wallet = document.getElementById("wallet-input")
+const Price = document.getElementById("price-input")
+
+Form2.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  const wallet = Number(Wallet.value)
+  const price = Number(Price.value)
+
+  const result1 = (wallet / price).toFixed(0)
+  
+  const result2 = (wallet - (price * result1).toFixed(2))
+
+  console.log("Ви отримаєте " + result1)
+  console.log("Ваш залишок " + result2)
+})
